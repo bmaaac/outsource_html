72b1841633ef851430c39e6c8dea8c6f0f15e23c
bmaaac <bmac42969@gmail.com>
2025-06-30 15:34:31 +0800

    vercel config

--- app.mjs
+++ app.mjs
@@ -4,25 +4,28 @@ import { fileURLToPath } from "url";
 import serverless from "serverless-http";
 
 const app = express();
-
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
+  console.log("Handling / route");
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.get("/careers", (req, res) => {
+  console.log("Handling /careers route");
   res.sendFile(path.join(__dirname, "public", "careers.html"));
 });
 
 app.get("/about", (req, res) => {
+  console.log("Handling /about route");
   res.sendFile(path.join(__dirname, "public", "about.html"));
 });
 
 // 404 handler
 app.get("*", (req, res) => {
+  console.log("Handling 404 route");
   res.status(404).send("404 - Not Found");
 });
 
